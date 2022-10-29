613ac415c19994b8f063337c17fa020f6a3a056d
Gabriel Lopes Rodrigues <gabriellopes@usp.br>
2022-10-29 18:54:49 -0300

    Put app name in the desktop notification

--- todoister/desktop_notify.py
+++ todoister/desktop_notify.py
@@ -2,4 +2,4 @@ import subprocess
 
 
 def notify(title, message):
-    subprocess.run(["notify-send", f"{title}", f"{message}"])
+    subprocess.run(["notify-send", f"{title}", f"{message}", "-a", f"Todoister"])
