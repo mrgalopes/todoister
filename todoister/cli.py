ec6a283a742c424adc02b2c05d19452d49b55a2f
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2023-11-20 19:52:43 -0300

    Uses click for the CLI

--- todoister/cli.py
+++ todoister/cli.py
@@ -1,4 +1,4 @@
-import sys
+import click
 
 from todoist_api_python.api import TodoistAPI
 
@@ -6,11 +6,9 @@ from todoister.desktop_notify import notify
 from todoister.api_key import get_api_key
 
 
-def add_task():
-    todo = sys.argv[1]
-    if todo == "":
-        exit(0)
-
+@click.command(help="Adds task to Todoist inbox")
+@click.argument('todo')
+def add_task(todo):
     api_key = get_api_key()
     api = TodoistAPI(api_key)
     try:
