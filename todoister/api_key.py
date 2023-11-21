456aba0bbd85b294ab635b03e241c04ddea06369
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2023-11-20 21:02:14 -0300

    refactor: Load API KEY

--- todoister/api_key.py
+++ todoister/api_key.py
@@ -5,18 +5,12 @@ from dotenv import dotenv_values
 
 from todoister.desktop_notify import notify
 
-DEFAULT_FILE = Path.home() / ".todoist_api_key"
+DEFAULT_API_KEY_FILE = Path.home() / ".todoist_api_key"
 
-def get_api_key():
-    api_key = get_api_key_from_file()
-    if api_key is not None:
-        return api_key
+def load_api_key():
+    if DEFAULT_API_KEY_FILE.exists():
+        return DEFAULT_API_KEY_FILE.read_text().strip()
     return get_api_key_from_env_file()
-
-def get_api_key_from_file():
-    if not DEFAULT_FILE.exists():
-        return None
-    return DEFAULT_FILE.read_text().strip()
 
 def get_api_key_from_env_file():
     dir_path = os.path.dirname(os.path.realpath(__file__))
--- todoister/cli.py
+++ todoister/cli.py
@@ -3,14 +3,13 @@ import click
 from todoist_api_python.api import TodoistAPI
 
 from todoister.desktop_notify import notify
-from todoister.api_key import get_api_key
+from todoister.api_key import load_api_key
 
 
 @click.command(help="Adds task to Todoist inbox")
 @click.argument('todo')
 def add_task(todo):
-    api_key = get_api_key()
-    api = TodoistAPI(api_key)
+    api = TodoistAPI(load_api_key())
     try:
         api.add_task(content=todo)
         notify("Todo added", f"{todo}")
