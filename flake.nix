8a95a31796772c9b61c910863166f81ed0981ffb
Gabriel Lopes Rodrigues <gabriellopes@outlook.com>
2023-12-26 14:10:32 -0300

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Simple Todoist task adder";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication;
+    in {
+      packages = {
+        todoister = mkPoetryApplication {
+          projectDir = ./.;
+          preferWheels = true;
+        };
+        default = self.packages.${system}.todoister;
+      };
+
+      devShells.default = pkgs.mkShell {
+        inputsFrom = [self.packages.${system}.todoister];
+        packages = [pkgs.poetry pkgs.ruff];
+      };
+    });
+}
